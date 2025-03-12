07954c03f2036c30f9be06597f1d45aeb7b29a2e
xiaozhuge123456 <xiaosasatuoge@163.com>
2025-03-12 15:47:37 +0800

    cloud相关组件

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -0,0 +1,41 @@
+package com.atguigu.cloud.controller;
+
+import com.atguigu.cloud.entities.Pay;
+import com.atguigu.cloud.entities.PayDTO;
+import com.atguigu.cloud.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @auther zzyy
+ * @create 2023-11-03 18:55
+ */
+@RestController
+public class PayController{
+    @Resource PayService payService;
+    @PostMapping(value = "/pay/add")
+    public String addPay(@RequestBody Pay pay){
+        System.out.println(pay.toString());
+        int i = payService.add(pay);
+        return "成功插入记录，返回值："+i;
+    }
+    @DeleteMapping(value = "/pay/del/{id}")
+    public Integer deletePay(@PathVariable("id") Integer id) {
+        return payService.delete(id);
+    }
+    @PutMapping(value = "/pay/update")
+    public String updatePay(@RequestBody PayDTO payDTO){
+        Pay pay = new Pay();
+        BeanUtils.copyProperties(payDTO, pay);
+
+        int i = payService.update(pay);
+        return "成功修改记录，返回值："+i;
+    }
+    @GetMapping(value = "/pay/get/{id}")
+    public Pay getById(@PathVariable("id") Integer id){
+        return payService.getById(id);
+    }//全部查询getall作为家庭作业
+}
