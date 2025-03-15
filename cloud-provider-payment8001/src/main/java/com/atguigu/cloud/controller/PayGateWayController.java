40779e10d432106c86004bd8b30dbce0562610d2
xiaozhuge123456 <xiaosasatuoge@163.com>
2025-03-15 21:41:37 +0800

    gateway服务网关相关测试

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayGateWayController.java
@@ -0,0 +1,37 @@
+package com.atguigu.cloud.controller;
+
+import cn.hutool.core.util.IdUtil;
+import com.atguigu.cloud.entities.Pay;
+import com.atguigu.cloud.resp.ResultData;
+import com.atguigu.cloud.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @auther zzyy
+ * @create 2023-11-20 13:09
+ */
+@RestController
+public class PayGateWayController
+{
+    @Resource
+    PayService payService;
+
+    @GetMapping(value = "/pay/gateway/get/{id}")
+    public ResultData<Pay> getById(@PathVariable("id") Integer id)
+    {
+        Pay pay = payService.getById(id);
+        return ResultData.success(pay);
+    }
+
+    @GetMapping(value = "/pay/gateway/info")
+    public ResultData<String> getGatewayInfo()
+    {
+        return ResultData.success("gateway info test："+ IdUtil.simpleUUID());
+    }
+}
